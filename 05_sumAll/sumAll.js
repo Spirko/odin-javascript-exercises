c597a7825962b9abd549a239986091a69ff4db45
Spirko <spirko@gmail.com>
2022-07-30 00:51:52 -0500

    completed sumAll using addition

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,8 @@
-const sumAll = function() {
-
+const sumAll = function(i1, i2) {
+  let sum=0;
+  if (i1<0 || i2<0 || !Number.isInteger(i1) || !Number.isInteger(i2)) return 'ERROR';
+  for(i=Math.min(i1,i2); i<=Math.max(i1,i2); i++) sum += i;
+  return sum;
 };
 
 // Do not edit below this line
