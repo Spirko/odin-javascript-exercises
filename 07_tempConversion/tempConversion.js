e534e0b69a391276e13d5d7df5d49ca24e8b0dbf
Spirko <spirko@gmail.com>
2022-07-30 01:06:54 -0500

    completed tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(t_f) {
+  return parseFloat(((t_f-32)*5/9).toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(t_c) {
+  return parseFloat((t_c * 9/5 + 32).toFixed(1));
 };
 
 // Do not edit below this line
