77282bd8c0a26b0216918f8f9de83834a0e1af52
Spirko <spirko@gmail.com>
2022-07-30 00:57:00 -0500

    completed sumAll with multiplication

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,7 +1,9 @@
 const sumAll = function(i1, i2) {
   let sum=0;
   if (i1<0 || i2<0 || !Number.isInteger(i1) || !Number.isInteger(i2)) return 'ERROR';
-  for(i=Math.min(i1,i2); i<=Math.max(i1,i2); i++) sum += i;
+  // for(i=Math.min(i1,i2); i<=Math.max(i1,i2); i++) sum += i;
+  let imin = Math.min(i1,i2);  let imax = Math.max(i1,i2);
+  sum = (imax*(imax+1) - imin*(imin-1))/2;
   return sum;
 };
 
