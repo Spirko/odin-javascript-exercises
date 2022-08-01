f05610151c9a66be57197b3ba4dfbf2537b98297
Spirko <spirko@gmail.com>
2022-08-01 01:52:55 -0500

    completed findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+  return arr.reduce((oldest, current) => age(current) > age(oldest)?current:oldest);
 };
+
+function age(person) {
+  return (('yearOfDeath' in person)?person.yearOfDeath:(new Date().getFullYear()))
+           - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
