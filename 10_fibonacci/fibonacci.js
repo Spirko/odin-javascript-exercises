8aaf34cad9dd1ffc8731ee6ac9395510ab7f9fb5
Spirko <spirko@gmail.com>
2022-08-01 01:38:06 -0500

    Completed fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+  if (n===1) return 1;
+  if (n===2) return 1;
+  n = parseInt(n);
+  if (n<0) return 'OOPS';
+  f0 = 0;
+  f1 = 1;
+  for(let i=1; i<n; i++) {
+    [f0, f1] = [f1, f0+f1];
+  }
+  return f1;
 };
 
 // Do not edit below this line
