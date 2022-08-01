1cbc946bc98e5e966f116d72c92cd68787bf0ecc
Spirko <spirko@gmail.com>
2022-08-01 01:15:14 -0500

    Completed calculator functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {	
+  return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(args) {
+  let result = 0;
+  for (a of args) {
+    result += parseInt(a);
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let result = 1;
+  for(let i of arr) result *= parseInt(i);
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	result = 1;
+  for(let i=1; i<=parseInt(a); i++) result *= i;
+  return result;
 };
 
 // Do not edit below this line
