00e48fc68dcc0a0f4dac0c9a0a9ecc6f2012095a
Spirko <spirko@gmail.com>
2022-08-01 01:31:13 -0500

    Completed palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+function isAlpha(s) {
+  return /^[a-zA-Z]$/.test(s);
+}
 
+const palindromes = function (s) {
+  let i=0; let j=s.length-1;
+  do {
+    while (!isAlpha(s[i])) i++;
+    while (!isAlpha(s[j])) j--;
+    if (i>=j) return true;
+    if (s[i].toLowerCase() != s[j].toLowerCase()) return false;
+    i++;
+    j--;
+  } while(true);
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
